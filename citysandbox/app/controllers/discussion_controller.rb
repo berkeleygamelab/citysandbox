b4620e5570b306e3367d9bab7a8d43d7a5300432
Stephanie Chou <stephchou921@gmail.com>
2011-10-01 15:59:03 -0700

    framework

---
+++ citysandbox/app/controllers/discussion_controller.rb
@@ -0,0 +1,14 @@
+class DiscussionController < ApplicationController
+  def summary
+  end
+
+  def specific
+  end
+
+  def ask
+  end
+
+  def challenge
+  end
+
+end
---
+++ citysandbox/app/controllers/user_controller.rb
@@ -0,0 +1,5 @@
+class UserController < ApplicationController
+  def profile
+  end
+
+end
