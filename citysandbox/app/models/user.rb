8f6ef7d1910cc813ff5cfd0a5373c126ba31dded
Stephanie Chou <berkeleygamelab@gmail.com>
2011-10-23 14:56:58 -0700

    individual questions

--- citysandbox/app/models/user.rb
+++ citysandbox/app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   belongs_to :followed_question
   has_many :questions
   has_many :responses
+  has_many :challenges
   has_many :followed_users
   has_many :followed_challenges
   has_many :followed_questions
