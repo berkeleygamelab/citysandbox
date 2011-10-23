bd6ce396bbc6b5bd4d6b121acdebeed0f696845b
Stephanie Chou <berkeleygamelab@gmail.com>
2011-10-23 16:01:06 -0700

    individual challenge

--- citysandbox/app/controllers/challenges_controller.rb
+++ citysandbox/app/controllers/challenges_controller.rb
@@ -14,6 +14,7 @@ class ChallengesController < ApplicationController
   # GET /challenges/1.json
   def show
     @challenge = Challenge.find(params[:id])
+    @category = Categories.find(@challenge.category_id)
 
     respond_to do |format|
       format.html # show.html.erb
