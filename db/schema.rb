bc27615e427622b9b054117ee0db591900008a83
Stephanie Chou <berkeleygamelab@gmail.com>
2011-11-03 00:39:11 -0700

    after db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(:version => 20111017000942) do
 
   create_table "challenges", :force => true do |t|
     t.integer  "question_id"
-    t.text     "description",         :limit => 1600
+    t.string   "description",         :limit => 1600
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title"
@@ -93,15 +93,15 @@ ActiveRecord::Schema.define(:version => 20111017000942) do
   create_table "messages", :force => true do |t|
     t.integer  "user_id"
     t.string   "subject"
-    t.text     "body"
+    t.string   "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "proposals", :force => true do |t|
     t.integer  "challenge_id",                                :null => false
-    t.text     "title",        :limit => 255,                 :null => false
-    t.text     "description",  :limit => 1600,                :null => false
+    t.string   "title",                                       :null => false
+    t.string   "description",  :limit => 1600,                :null => false
     t.integer  "rating",                       :default => 0, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -123,7 +123,7 @@ ActiveRecord::Schema.define(:version => 20111017000942) do
   create_table "response_challenges", :force => true do |t|
     t.integer  "challenge_id",                                :null => false
     t.integer  "user_id",                                     :null => false
-    t.text     "response",     :limit => 1600,                :null => false
+    t.string   "response",     :limit => 1600,                :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "anonymous",                    :default => 0
@@ -132,7 +132,7 @@ ActiveRecord::Schema.define(:version => 20111017000942) do
   create_table "response_events", :force => true do |t|
     t.integer  "event_id",                                  :null => false
     t.integer  "user_id",                                   :null => false
-    t.text     "response",   :limit => 1600,                :null => false
+    t.string   "response",   :limit => 1600,                :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "anonymous",                  :default => 0
@@ -141,18 +141,18 @@ ActiveRecord::Schema.define(:version => 20111017000942) do
   create_table "response_questions", :force => true do |t|
     t.integer  "question_id",                                :null => false
     t.integer  "user_id",                                    :null => false
-    t.text     "response",    :limit => 1600,                :null => false
+    t.string   "response",    :limit => 1600,                :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "anonymous",                   :default => 0
   end
 
   create_table "users", :force => true do |t|
-    t.text     "login",           :limit => 75,                   :null => false
-    t.text     "picture",         :limit => 255
+    t.string   "login",           :limit => 75,                  :null => false
+    t.string   "picture"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text     "email",           :limit => 75,  :default => " ", :null => false
+    t.string   "email",           :limit => 75, :default => " ", :null => false
     t.string   "password_digest"
     t.string   "location"
   end
