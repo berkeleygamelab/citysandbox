915b8ba3c151e8bf555d06cc0521e995705ed59c
Ian Norris <ian_norris@berkeley.edu>
2011-11-20 14:00:37 -0800

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ActiveRecord::Base
   validates :email, :presence => true
   validates :password, :presence => true
   validates :location, :presence => true
-  validate :name_check
+  #validate :name_check
   
   def name_check
     if(login.strip != login)
