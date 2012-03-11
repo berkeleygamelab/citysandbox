04b0f0205d23d203cc6110a6599eed55933b4cd3
Ian Norris <ian_norris@berkeley.edu>
2012-03-11 16:10:18 -0700

    fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,9 +3,8 @@ class UserMailer < ActionMailer::Base
   def signup_notification(user, randomValue)
       recipients "#{user.login} <#{user.email}>"
       from       "appcivist "
-      subject    "Please activate your new account"
+      subject    "Please activate your new account [AppCivist]"
       sent_on    Time.now
-      body      "check it out! go to this link and yeah you'll be awesome! "
-      #http://citysandbox.heroku.com/validate?token=#{randomValue}&user=#{user.id}
+      body      "Thanks for registering with AppCivist! Please go to this URL to verify your account: http://citysandbox.heroku.com/validate?token=#{randomValue}&user=#{user.id}"
     end
 end
