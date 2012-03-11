3edeedb776ead96d8c18d54416c11eda367b8353
Ian Norris <ian_norris@berkeley.edu>
2012-03-11 15:51:57 -0700

    playing around with user auth

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ActionMailer::Base
       from       "appcivist "
       subject    "Please activate your new account"
       sent_on    Time.now
-      body       {"check it out! go to this link and yeah you'll be awesome! "}
+      text       {"check it out! go to this link and yeah you'll be awesome! "}
       #http://citysandbox.heroku.com/validate?token=#{randomValue}&user=#{user.id}
     end
 end
