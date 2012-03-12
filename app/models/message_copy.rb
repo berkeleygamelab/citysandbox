02feca0b4c79cdc393e4a5e2f35e5b220e5a3acd
Samuel P. Fendell <sfendell@berkeley.edu>
2012-03-11 17:23:31 -0700

    force

--- app/models/message_copy.rb
+++
@@ -1,9 +0,0 @@
-class MessageCopy < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :message
-  belongs_to :folder
-
-  validate :user_id, :presence => true
-  validate :message_id, :presence => true
-  
-end
