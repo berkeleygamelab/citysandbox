7cd3cddb294123605ac87d814a63ef27c8032ba2
Ian Norris <ian_norris@berkeley.edu>
2012-03-18 10:29:23 -0700

    what

--- db/migrate/20120311210112_add_type_forgot_p_wto_users.rb
+++ db/migrate/20120311210112_add_type_forgot_p_wto_users.rb
@@ -1,11 +1,11 @@
 class AddTypeForgotPWtoUsers < ActiveRecord::Migration
   def up
-      add_column :Users, :type, :varchar(20)
-      add_column :Users, :temp_pw, :integer
+      add_column :users, :type, :varchar(20)
+      add_column :users, :temp_pw, :integer
   end
 
   def down
-      remove_column :Users, :type, :varchar(20)
-      remove_column :Users, :temp_pw, :integer
+      remove_column :users, :type, :varchar(20)
+      remove_column :users, :temp_pw, :integer
   end
 end
