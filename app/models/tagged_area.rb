58e61ea65abaca3f678861416bd56c5a7ac0b05f
Ian_Vincent_Norris <cs186-nw@hive1.cs.berkeley.edu>
2012-03-23 15:59:01 -0700

    being clever

--- app/models/tagged_area.rb
+++ app/models/tagged_area.rb
@@ -1,5 +1,5 @@
 class TaggedArea < ActiveRecord::Base
 	has_many :groups, :through => :group_areas
-	has_many :coordinates
+	has_many :coordinates, :foreign_key => "tagged area_id"
 #	has_many :users, :through => user_areas
 end
