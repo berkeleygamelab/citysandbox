87ce1a6f544a5c15c3e96ca5561e922e25113c69
Ian_Vincent_Norris <cs186-nw@hive1.cs.berkeley.edu>
2012-03-23 15:20:30 -0700

    yay

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -19,28 +19,29 @@ class MapController < ApplicationController
       puts name
       
       coordinates = params["coordinates"]
-      puts coordinates;
-      coordinates = coordinates.split(",")
+      puts coordinates
+      #coordinates = coordinates.split(",")
       #coordinates.gsub(//, " ")
      
-    	  puts coordinates
+      # puts coordinates
   
       taggedarea = TaggedArea.new(:title => name)
       
       #make sure areas are saving
       if taggedarea.save
           #all coordinates associated with this tagged area based on id
-          coordinates = params["coordinates"]
-        end
+          
+        
 	  #treating coordinates as a LIST OF TUPLES 
 	     
-          coordinates.each do |t|
-	      c = t[0] + "," + t[1]
+          coordinates.each do |set|
+	      t = set.split(",")
+              c = t[0] + "," + t[1]
               newcoord = Coordinate.new(:tagged_area_id => taggedarea.id, :location => c)
 	      #MAKE SURE THAT COORDS ARE SAVING
 	      newcoord.save
 	  end
-      
+       end
    end
    
    def draw_map
