262d32a170a1d046bf9cafd17e50371a2c9f41b2
Ian_Vincent_Norris <cs186-nw@hive1.cs.berkeley.edu>
2012-03-25 12:49:08 -0700

    fix

--- db/migrate/20120325054143_remove_qmark.rb
+++ db/migrate/20120325054143_remove_qmark.rb
@@ -10,24 +10,24 @@ class RemoveQmark < ActiveRecord::Migration
 	#remove_column :notifications, :seen?
 	remove_column :coordinates, "tagged area_id"
 	
-	add_column :sent_messages, :anonymous
-	add_column :response_templates, :anonymous
-	add_column :response_templates, :group_response
-	add_column :sent_messages, :group_message
-	add_column :received_messages, :read
-	add_column :questions, :anonymous
-	add_column :notifications, :seen
-	add_column :coordinates, :tagged_area_id
+	add_column :sent_messages, :anonymous, :boolean
+	add_column :response_templates, :anonymous, :boolean
+	add_column :response_templates, :group_response, :boolean
+	add_column :sent_messages, :group_message, :boolean
+	add_column :received_messages, :read, :boolean
+	add_column :questions, :anonymous, :boolean
+	add_column :notifications, :seen, :boolean
+	add_column :coordinates, :tagged_area_id, :boolean
   end
 
   def down
-	remove_column :sent_message, :anonymous, :boolean
-	remove_column :response_template, :anonymous, :boolean
-	remove_column :response_template, :group_response, :boolean
-	remove_column :sent_message, :group_message, :boolean
-	remove_column :received_message, :read, :boolean
-	remove_column :question, :anonymous, :boolean
-	remove_column :notification, :seen, :boolean
+	remove_column :sent_message, :anonymous
+	remove_column :response_template, :anonymous
+	remove_column :response_template, :group_response
+	remove_column :sent_message, :group_message
+	remove_column :received_message, :read
+	remove_column :question, :anonymous
+	remove_column :notification, :seen
 	
 	add_column :sent_message, :anonymous?
 	add_column :response_template, :anonymous?
