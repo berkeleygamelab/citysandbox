d1c47ac1b4ec95651e4e3429aafdd76f9e28ef70
Ian_Vincent_Norris <cs186-nw@hive1.cs.berkeley.edu>
2012-03-29 00:29:24 -0700

    matt

--- db/migrate/20120329072055_meh.rb
+++ db/migrate/20120329072055_meh.rb
@@ -1,6 +1,6 @@
 class Meh < ActiveRecord::Migration
   def up
-  add_column :challenges, :voting_deadline, :datetime
+  #add_column :challenges, :voting_deadline, :datetime
   add_column :challenges, :submission_deadline, :datetime
   end
 
