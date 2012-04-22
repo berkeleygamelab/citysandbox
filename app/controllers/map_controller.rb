102254e73684c5c41c1093e55e37ab1637c519e4
Ian Norris <ian_norris@berkeley.edu>
2012-04-22 15:21:08 -0700

    ergh

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -111,7 +111,7 @@ class MapController < ApplicationController
     @areas = []
     i = 0
     TaggedArea.all.each do |set|
-      @areas.push(set.coordinates.map(&:location).map{|item| item.split(", ")})
+      @areas.push(set.coordinates.map(&:location).map{|item| item[1..item.length-2]}.map{|item| item.split(", ")})
       puts @areas[i]
       puts @areas[i].class
       puts @areas[i][0].class
