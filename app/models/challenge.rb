7f90d34c2cfbe7b04f5cdd82b975534d4874c109
saasbook <saasbook@saasbook.(none)>
2012-05-02 01:00:14 -0700

    test

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,33 +2,33 @@ class Challenge < ActiveRecord::Base
   has_many :proposals
 
   has_many :events
-  acts_as :item_template, :as => :producible
- 
-	
+  #acts_as :item_template, :as => :producible
+
+
   belongs_to :question
   before_save :identify_type
 
   validates :submission_deadline, :presence => true
   validates :vote_deadline, :presence => true
-  
+
   scope :followed, lambda{|key| {:conditions => {:id => key}}}
-  
+
   scope :has_category,       lambda{ |n| { :conditions => { :categories_id => n}}}
   scope :has_title, lambda{|name| {:conditions => ["title LIKE ? OR title LIKE ? OR title LIKE ?", "% " + name + " %", name, name + " %"]}}
   scope :keyword, lambda{|key| {:conditions => ["title LIKE ? OR title LIKE ? OR title LIKE ? OR description LIKE ? OR description LIKE ? OR description LIKE ?", "% " + key + " %", key, key + " %", "% " + key + " %", key, key + " %" ]}}
 
   def identify_type
-	type = "Challenge"
+  type = "Challenge"
   end
 
-    ##copied to item_template  
+    ##copied to item_template
 
 #   def create_followed
 #     followed = FollowedChallenge.new
 #     followed.user_id = current_user.id
 #     followed.challenge_id = id
-     
-     
+
+
 #     followed.save
 #   end
 #   def create_followed(follower)
@@ -40,20 +40,20 @@ class Challenge < ActiveRecord::Base
 #     end
 #   end
  ##
-	def generate_content
-		return {"question_id"=>question_id, "submission_deadline"=>submission_deadline,"vote_deadline" => vote_deadline, "minimum_to_run" => minimum_to_run}
-	end
- 
+  def generate_content
+    return {"question_id"=>question_id, "submission_deadline"=>submission_deadline,"vote_deadline" => vote_deadline, "minimum_to_run" => minimum_to_run}
+  end
+
    def most_popular(since_last)
      return challenge.where("updated_at > '#{since_last}'").where()
    end
-   
+
    def remove_followed
       followed = followed_challenges
       a= followed.where(:user_id => current_user.id).first
       return a.destroy
     end
-    
+
     def remove_followed(follower)
       followed = followed_challenges
       a = followed.where(:user_id => follower.id).first
@@ -63,7 +63,7 @@ class Challenge < ActiveRecord::Base
    def fetch_location
      @challenges_table = ENV['csb_locations']
      return ::FT.execute "SELECT Location FROM #{@challenges_table} WHERE ID = #{id} AND Origin = 'challenges'"
-   end    
+   end
 
    def update_location(loc)
      @challenges_table = ENV['csb_locations']
@@ -72,25 +72,25 @@ class Challenge < ActiveRecord::Base
        return insert_location(loc)
      end
      @rowid = @quest_dummy[0][:rowid]
-     
+
      return ::FT.execute "UPDATE #{@challenges_table} SET Location='#{loc}' WHERE ROWID = '#{@rowid}' AND Origin = 'challenges'"
    end
-   
+
    def insert_location(loc)
      @challenges_table = ENV['csb_locations']
      return ::FT.execute "INSERT INTO #{@challenges_table} (Location, ID, Origin, Category) VALUES ('#{loc}', #{id}, 'challenges', #{categories_id})"
    end
-   
+
    def grab_nearest(number)
      @challenges_table = ENV['csb_locations']
      @loc_x = location.split[0].to_f
      @loc_y = location.split[1].to_f
      return ::FT.execute "SELECT * FROM #{@challenges_table} WHERE Origin = 'challenge' ORDER BY ST_DISTANCE(Location, LATLNG(#{@loc_x},#{@loc_y})) LIMIT #{number}"
    end
-   
+
    #grabs the nearest locations by distance and location
    def grab_nearest_by_location(distance, loc)
-     
+
    end
    def grab_circle(distance, target_loc, number)
      @challenges_table = ENV['csb_locations']
@@ -99,7 +99,7 @@ class Challenge < ActiveRecord::Base
      puts distance
      return ::FT.execute "SELECT * FROM #{@challenges_table} WHERE ST_INTERSECTS(Location, CIRCLE(LATLNG(#{@loc_x}, #{@loc_y}), #{distance})) AND Origin = 'challenges'"
    end
-   
+
    def most_popular_proposal
      my_proposals = proposals
      my_counts = -1
@@ -115,7 +115,7 @@ class Challenge < ActiveRecord::Base
      end
      return proposals.where(:id => my_popular)
    end
-   
+
     def retrieve_google(db_return)
        rtn = []
        db_return.each do |x|
@@ -123,14 +123,14 @@ class Challenge < ActiveRecord::Base
        end
        return Challenge.followed(rtn)
      end
-   
-   
+
+
     def most_popular(since_last, distance, target_location)
        google_set = grab_circle(distance, target_location, 25)
        google_fetch = retrieve_google(google_set)
        return google_fetch.where("updated_at > '#{since_last}'").order("popularity DESC")
      end
-   
+
       def sift_circle(distance, target_loc, number, set)
          circles = grab_circle(distance, target_loc, number)
          circles = retrieve_google_with_set(set, circles)
@@ -144,6 +144,6 @@ class Challenge < ActiveRecord::Base
          end
          return set.followed(rtn)
        end
-   
-   
+
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,21 +1,21 @@
 class Event < ActiveRecord::Base
   belongs_to :challenge
-  acts_as :item_template, :as => :producible
-  
+  #acts_as :item_template, :as => :producible
+
 #  belongs_to :question
 #  belongs_to :user
 #  belongs_to :categories
 #  belongs_to :challenge
 #  attr_accessor :lat
 #  attr_accessor :lng
-  
+
 #  validates :categories_id, :presence => true
-  
+
 #  has_many :response_events
 #  has_many :followed_events
-  
+
   scope :followed, lambda{|key| {:conditions => {:id => key}}}
-  
+
   scope :has_category,       lambda{ |n| { :conditions => { :categories_id => n}}}
   scope :has_title, lambda{|name| {:conditions => ["title LIKE ? OR title LIKE ? OR title LIKE ?", "% " + name + " %", name, name + " %"]}}
   scope :keyword, lambda{|key| { :conditions => ["title LIKE ? OR title LIKE ? OR title LIKE ? OR description LIKE ? OR description LIKE ? OR description LIKE ?", "% " + key + " %", key, key + " %", "% " + key + " %", key, key + " %" ]}}
@@ -25,9 +25,9 @@ class Event < ActiveRecord::Base
   def category_id
      return categories_id
   end
-  
+
   def generate_content
-	return {"time"=>time, "challenge_id"=>challenge_id, "minimum_to_run"=>minimum_to_run}
+  return {"time"=>time, "challenge_id"=>challenge_id, "minimum_to_run"=>minimum_to_run}
   end
 
   def create_followed
@@ -36,7 +36,7 @@ class Event < ActiveRecord::Base
     followed.event_id = id
     followed.save
   end
-  
+
   def create_followed(follower)
     followed = FollowedEvent.new
     followed.user_id = follower.id
@@ -45,27 +45,27 @@ class Event < ActiveRecord::Base
       followed.save
     end
   end
-  
+
   def most_popular(since_last)
     return Question.where("updated_at > '#{since_last}'").where()
   end
-  
+
   def remove_followed
      followed = followed_events
      a= followed.where(:user_id => current_user.id).first
      return a.destroy
    end
-   
+
    def remove_followed(follower)
      followed = followed_events
      a = followed.where(:user_id => follower.id).first
      return a.destroy
    end
-   
+
    def fetch_location
      @events_table = ENV['csb_locations']
      return ::FT.execute "SELECT Location FROM #{@events_table} WHERE ID = #{id} AND Origin = 'events'"
-   end    
+   end
 
    def update_location(loc)
      @events_table = ENV['csb_locations']
@@ -74,23 +74,23 @@ class Event < ActiveRecord::Base
        return insert_location(loc)
      end
      @rowid = @quest_dummy[0][:rowid]
-     
+
      return ::FT.execute "UPDATE #{@events_table} SET Location='#{loc}' WHERE ROWID = '#{@rowid}' AND Origin = 'events'"
    end
-   
+
    def insert_location(loc)
      @events_table = ENV['csb_locations']
      return ::FT.execute "INSERT INTO #{@events_table} (Location, ID, Origin, Category) VALUES ('#{loc}', #{id}, 'events', #{categories_id})"
    end
-   
-   
+
+
    def grab_nearest(number)
      @events_table = ENV['csb_locations']
      @loc_x = location.split[0].to_f
      @loc_y = location.split[1].to_f
      return ::FT.execute "SELECT * FROM #{@events_table} WHERE Origin = 'events' ORDER BY ST_DISTANCE(Location, LATLNG(#{@loc_x},#{@loc_y})) LIMIT #{number}"
    end
-   
+
     def retrieve_google(db_return)
        rtn = []
        db_return.each do |x|
@@ -98,15 +98,15 @@ class Event < ActiveRecord::Base
        end
        return Event.followed(rtn)
      end
-   
-   
+
+
     def most_popular(since_last, distance, target_location)
        set =  Event.where("updated_at > '#{since_last}'").order("popularity DESC")
        google_set = grab_circle(distance, target_location, 25)
        google_fetch = retrieve_google(google_set)
        return google_fetch.where("updated_at > '#{since_last}'").order("popularity DESC")
      end
-     
+
       def sift_circle(distance, target_loc, number, set)
          circles = grab_circle(distance, target_loc, number)
          circles = retrieve_google_with_set(set, circles)
@@ -120,6 +120,6 @@ class Event < ActiveRecord::Base
          end
          return set.followed(rtn)
        end
-     
-   
+
+
 end
