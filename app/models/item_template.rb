f3ab6aa29d0acc9fe5e5145f4209f4642c56df12
saasbook <saasbook@saasbook.(none)>
2012-05-02 01:10:18 -0700

    cray cray

--- app/models/item_template.rb
+++ app/models/item_template.rb
@@ -98,7 +98,7 @@ class ItemTemplate < ActiveRecord::Base
       @lng = ItemTemplate.location.split(',')[1].to_f
     resultHash = {}
     typearray.each do |type|
-      resultHash[type] = (::FT.execute "SELECT * FROM #{@table} WHERE Type = #{type} ORDER BY ST_DISTANCE(Location, LATLNG(#{@lat},#{@lng})) LIMIT #{limit}"))
+      resultHash[type] = ::FT.execute "SELECT * FROM #{@table} WHERE Type = #{type} ORDER BY ST_DISTANCE(Location, LATLNG(#{@lat},#{@lng})) LIMIT #{limit}"
       end
     return resultHash
   end
@@ -114,7 +114,7 @@ class ItemTemplate < ActiveRecord::Base
       @lng = target_loc.split[1].to_f
     resulthash = {}
     typearray.each do |x|
-      resulthash[x] = (::FT.execute "SELECT * FROM #{@table} WHERE ST_INTERSECTS(Location, CIRCLE(LATLNG(#{@lat}, #{@lng}), #{radius})) AND Type = '#{x}' "))
+      resulthash[x] = ::FT.execute "SELECT * FROM #{@table} WHERE ST_INTERSECTS(Location, CIRCLE(LATLNG(#{@lat}, #{@lng}), #{radius})) AND Type = '#{x}' "
       end
     return resulthash
   end
@@ -183,7 +183,7 @@ class ItemTemplate < ActiveRecord::Base
   #def grab_circle(distance, target_loc)
    #  @events_table = ENV['csb_locations']
     # @loc_x = target_loc.split[0].to_f
-     #@loc_y = target_loc.split[1].to_f
+     # @loc_y = target_loc.split[1].to_f
      #return ::FT.execute "SELECT * FROM #{@events_table} WHERE ST_INTERSECTS(Location, CIRCLE(LATLNG(#{@loc_x}, #{@loc_y}), #{distance}))"
   #end
 
