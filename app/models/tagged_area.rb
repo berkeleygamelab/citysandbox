80572155ca8f90d21235d443b8393c84a915d783
Ian Norris <ian_norris@berkeley.edu>
2012-05-02 18:56:45 -0700

    whee

--- app/models/tagged_area.rb
+++ app/models/tagged_area.rb
@@ -18,6 +18,7 @@ class TaggedArea < ActiveRecord::Base
      lineEquation = lineEquation[0..lineEquation.length-2]
         arrayOfIds = []
      type.each do |x|
+       puts "SELECT * FROM #{@table} WHERE ST_INTERSECTS(Location, POLYGON(#{lineEquation}) AND Type = '#{x}' "
      hashSet = ::FT.execute "SELECT * FROM #{@table} WHERE ST_INTERSECTS(Location, POLYGON(#{lineEquation}) AND Type = '#{x}' "
        hashSet.each do |set|
           arrayOfIds += set["ID"]
