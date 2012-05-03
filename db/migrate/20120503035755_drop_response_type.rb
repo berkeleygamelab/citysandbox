2f9e74e0aec9a6c194a12041099ede3a981a84ce
saasbook <saasbook@saasbook.(none)>
2012-05-02 21:02:29 -0700

    migration fix

--- db/migrate/20120503035755_drop_response_type.rb
+++ db/migrate/20120503035755_drop_response_type.rb
@@ -2,7 +2,7 @@ class DropResponseType < ActiveRecord::Migration
   def up
     remove_column :response_templates, :responder_id
     add_column :response_templates, :user_id, :integer
-    remove_column :type
+    remove_column :response_templates, :type
 
   end
 
