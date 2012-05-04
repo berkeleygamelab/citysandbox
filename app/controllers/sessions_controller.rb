a497b5c84c2c48dc120d4b4c532b85a0345e077f
Ian Norris <ian_norris@berkeley.edu>
2012-05-03 22:28:20 -0700

    lols

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url
+    redirect_to :controller => :home, :action => :splash
   end
 
   def new
