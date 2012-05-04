3d4730e16aff8e9efb2b674e9fbfaef36c4d2de5
Ian Norris <ian_norris@berkeley.edu>
2012-05-04 03:47:02 -0700

    lls

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -86,7 +86,7 @@ class UsersController < ApplicationController
   def profile
     @user = User.find(params[:id])
    # @followed_user = current_user.followees.where(:followee_id => @user_id).size != 0
-    @items = ItemTemplate.where(:user_id +> params[:id])
+    @items = ItemTemplate.where(:user_id => params[:id])
   end
   
   respond_to :json
