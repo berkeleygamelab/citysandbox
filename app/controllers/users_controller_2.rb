cc1458e5a99803f1854a5c0f715dd1fac1a267c9
Ian Norris <ian_norris@berkeley.edu>
2012-05-04 09:07:46 -0700

    fix

--- app/controllers/users_controller_2.rb
+++
@@ -1,8 +0,0 @@
-class UsersController < ApplicationController  
-  def random_string(len)
-    chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
-     newpass = ""
-     1.upto(len) { |i| newpass << chars[rand(chars.size-1)] }
-     return newpass
-  end
-end
