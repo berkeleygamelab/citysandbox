078ae9bb8383fa5fb21e6f6f6ba38f7ab4234241
Ian Norris <ian_norris@berkeley.edu>
2012-05-04 07:47:43 -0700

    lols

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -33,8 +33,8 @@ class ChallengesController < ApplicationController
 
   def challengeNew
     @challenge = Challenge.new
-    @sample = Question.all.pop
-    @samples = @sample.kludgy_related_similar()
+    @category =  Category.new
+    puts "eh"
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @challenge }
